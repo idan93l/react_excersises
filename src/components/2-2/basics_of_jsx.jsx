e1724933a9682b74ff96e80bab824eb97af0d87d
Idan Lev <idan.93lev@gmail.com>
2021-12-12 14:37:09 +0200

    first commit

---
+++ src/components/2-2/basics_of_jsx.jsx
@@ -0,0 +1,9 @@
+// const helloWorld = () => {
+//   return (
+//     <div>
+//       <h1>Hello World!</h1>
+//       <a href="#">Click me!!!</a>
+//     </div>
+//   );
+// };
+// export default App;
