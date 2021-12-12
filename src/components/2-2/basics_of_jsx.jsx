3865b1927316fe4dfb933cbfaeb4edf4ea155b0b
Idan Lev <idan.93lev@gmail.com>
2021-12-12 23:18:43 +0200

    2-2

--- src/components/2-2/basics_of_jsx.jsx
+++ src/components/2-2/basics_of_jsx.jsx
@@ -1,9 +1,15 @@
-// const helloWorld = () => {
-//   return (
-//     <div>
-//       <h1>Hello World!</h1>
-//       <a href="#">Click me!!!</a>
-//     </div>
-//   );
-// };
-// export default App;
+const data = ["hello", "world"];
+const number1 = 5;
+const number2 = 6;
+const string = "I love React!"
+
+const HelloWorld = () => {
+  return (
+    <div>
+      <h1>{`${data[0]} ${data[1]}`}</h1>
+      <h1>{`${number1} + ${number2}`}</h1>
+      <h1>{`The string's length is ${string.length}`}</h1>
+    </div>
+  );
+};
+export default HelloWorld;
